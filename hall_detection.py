bab73e1ba28acf7d00c59434304d87874a58e5e9
Belen96x <belen.saavedra.39@gmail.com>
2025-05-01 12:34:23 +0200

    First prompt version and data preprocessing

---
+++ hall_detection.py
@@ -0,0 +1,30 @@
+prompt = ''' 
+
+You are an expert translator of Spanish and English. Your task will be to assess if the translations pairs you are presented with contain hallucinations or not. An hallucination is a translation that contains information completely unrelated to the source.
+
+PROCESS:
+
+1. Read carefully the sentence pairs 
+2. Identify if hallucinations are present 
+3. If present, mark the hallucinations between <<< >>> 
+4. Classify the hallucinations following this criteria:
+    4.1 1_No_hallucination: There is no hallucination present
+    4.2 2_Small_hallucination: Only 1 or 2 words are hallucinated
+    4.3 3_Partial_hallucination: More than 3 words are hallucinated
+    4.4 4_Full_hallucination: only 1 or 2 words are NOT hallucinated
+
+OUTPUT_FORMAT:
+
+EXAMPLE:
+
+Source sentence: Si deseas aprender cómo lanzar un búmeran y que vuelva a su mano, asegúrate de contar con uno que sea adecuado para el regreso.
+
+Target translation: If you want to learn how to throw a buffalo and get it back into your hand, make sure you have one that's suitable for the return
+
+Output: 
+"If you want to learn how to throw a <<<buffalo>>> and get it back into your hand, make sure you have one that's suitable for the return."
+
+2_Small_hallucination
+
+
+'''
---
+++ readme.md
@@ -0,0 +1,11 @@
+---------
+TO - DO :
+---------
+
+
+1. Idioma elegido - Inglés, español y español, inglés. - DONE
+2. Limpiar datos y hacer subselección - DONE (Limpiar más?)
+3. Elegir los modelos (2 LLMs)
+4. Diseñar el prompt para detectar alucinaciones (Al momento de ejecutarlo, tenemos que sacarle las <<< >>>. Hay una columa con traducción pura no marcada). Pedirle que ponga la parte alucinada entre <<< >>>, comparar los fragmentos detectados. - Done (Pulirlo cuando esté la clase para la ejecución)
+5. Comparar el resultado del modelo con la human annotation del dataset.
+6. Calcular las métricas de performance
